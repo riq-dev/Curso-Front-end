463cead317ebcf570c6355ee2437ec6f38e30fe8
riq-dev <filth.999@protonmail.ch>
2021-09-13 07:08:10 -0400

    update

--- Modules/05 - JavaScript Básico/Aula 10 - Condicional IF/script.js
+++ Modules/05 - JavaScript Básico/Aula 10 - Condicional IF/script.js
@@ -1,14 +1,12 @@
 var idade = 37
 
-if (idade >= 35){
-    console.log('pode')
-    console.log('Qual o seu pedido?')
-}
-else if (idade >= 18){
-    console.log('pode')
-    console.log('Mostre a identidade')
-}
-else{
-    console.log('não pode')
-    console.log('Volte futuramente')
+if (idade >= 35) {
+    console.log("pode")
+    console.log("Qual o seu pedido?")
+} else if (idade >= 18) {
+    console.log("pode")
+    console.log("Mostre a identidade")
+} else {
+    console.log("não pode")
+    console.log("Volte futuramente")
 }
