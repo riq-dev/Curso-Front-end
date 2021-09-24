09efe881811f5c1a5027f1a7ac0221592233d714
riq-dev <filth.999@protonmail.ch>
2021-09-23 20:23:39 -0400

    update

--- Modules/05 - JavaScript Básico/Aula 14 - Arrays/script.js
+++ Modules/05 - JavaScript Básico/Aula 14 - Arrays/script.js
@@ -1,25 +1,25 @@
-var alunos = ['Martins', 'Igor', 'José', 'Marcos', 'Mariana', 'Mariana']
-console.log(alunos.length)
-console.log(alunos[0])
+var alunos = ["Martins", "Igor", "José", "Marcos", "Mariana", "Mariana"];
+console.log(alunos.length);
+console.log(alunos[0]);
 
-var alunos2 = ['Martins', 'Igor', 'José', 'Marcos', 'Mariana', 'Mariana']
+var alunos2 = ["Martins", "Igor", "José", "Marcos", "Mariana", "ariana"];
 
 // Irá mostrar o valor dos índices, que nesse exemplo, são nomes.
 for (var i = 0; i < alunos2.length; i++) {
-    console.log(alunos2[i])
+    console.log(alunos2[i]);
 }
 
 // Irá retornar o índice dos elementos.
 for (var i in alunos2) {
-    console.log(i)
+    console.log(i);
 }
 
 // Irá mostrar o valor dos índices, que nesse exemplo, são nomes.
 for (var i of alunos2) {
-    console.log(i)
+    console.log(i);
 }
 
 // Irá mostrar o valor dos índices, que nesse exemplo, são nomes.
-alunos.forEach(function (elemento) {
-    console.log(elemento)
+alunos.forEach(function(elemento) {
+    console.log(elemento);
 })
--- Modules/05 - JavaScript Básico/Aula 15 - Funções_Parte 1/script.js
+++ Modules/05 - JavaScript Básico/Aula 15 - Funções_Parte 1/script.js
@@ -1,12 +1,11 @@
 function media(n1, n2) {
-    var nota1 = n1
-    var nota2 = n2
-    var media = (nota1 + nota2) / 2
-    //console.log(media)
-
-    return media
+    var nota1 = n1;
+    var nota2 = n2;
+    var media = (nota1 + nota2) / 2;
+    return media;
 }
-var resultado1 = media(6,7)
-var resultado2 = media(8,7)
 
-console.log(resultado1 + ' e ' + resultado2)
+var resultado1 = media(6, 7);
+var resultado2 = media(8, 7);
+
+console.log(resultado1 + "e" + resultado2);
