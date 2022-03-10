0bd7b9c094d525efeacc2ff5e24dfb78316b8687
Luís Henrique <filth.999@protonmail.ch>
2022-03-10 15:28:23 -0400

    Add files via upload
    update

---
+++ Módulos-curso/05 - Introdução-JavaScript/js/main.js
@@ -0,0 +1,16 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var imc = peso / (altura * altura);
+
+tdImc.textContent = imc;
