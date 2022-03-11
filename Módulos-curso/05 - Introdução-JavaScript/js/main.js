9915bc18c2933ea74a9e6b3cc66ea15fb9300151
Luís Henrique <filth.999@protonmail.ch>
2022-03-11 11:31:52 -0400

    update

--- Módulos-curso/05 - Introdução-JavaScript/js/main.js
+++ Módulos-curso/05 - Introdução-JavaScript/js/main.js
@@ -1,8 +1,5 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Nutricionista";
-
-var adicionar = document.querySelector("#adicionar-paciente");
-
 var pacientes = document.querySelectorAll(".paciente");
 
 for (var i = 0; i < pacientes.length; i++) {
@@ -44,7 +41,36 @@ for (var i = 0; i < pacientes.length; i++) {
 
 }
 
-adicionar.addEventListener("click", function(e) {
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(e) {
     e.preventDefault();
-    console.log("Olá, eu fui clicado!");
+
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+
+    var pacienteTr = document.createElement("tr");
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
 });
