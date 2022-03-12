705060df2f1bd47676b29a974cf109c4f0d922ef
riq-dev <luis.riq.henrique@gmail.com>
2022-03-12 16:55:43 -0400

    atualização no remove paciente

--- Módulos-curso/05 - Introdução-JavaScript/js/remover.js
+++ Módulos-curso/05 - Introdução-JavaScript/js/remover.js
@@ -1,7 +1,14 @@
 var pacientes = document.querySelectorAll(".paciente");
 
-pacientes.forEach(function(paciente) {
-    paciente.addEventListener("dblclick", function() {
-        this.remove();
-    });
+var tabela = document.querySelector("#tabela-pacientes");
+
+tabela.addEventListener("dblclick", function(event) {
+    event.target.parentNode.remove();
 });
+
+
+// pacientes.forEach(function(paciente) {
+//     paciente.addEventListener("dblclick", function() {
+//         this.remove();
+//     });
+// });
