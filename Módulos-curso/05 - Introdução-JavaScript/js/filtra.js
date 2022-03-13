cb62b58ef8c41e39becf763c7ff63144e6eda933
riq-dev <luis.riq.henrique@gmail.com>
2022-03-13 14:02:37 -0400

    update search bar

--- Módulos-curso/05 - Introdução-JavaScript/js/filtra.js
+++ Módulos-curso/05 - Introdução-JavaScript/js/filtra.js
@@ -9,8 +9,9 @@ campoFiltro.addEventListener("input", function() {
             var paciente = pacientes[i];
             var tdNome = paciente.querySelector(".info-nome");
             var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value, "i");
 
-            if (nome != this.value) {
+            if (!expressao.test(nome)) {
                 paciente.classList.add("invisivel");
             } else {
                 paciente.classList.remove("invisivel");
