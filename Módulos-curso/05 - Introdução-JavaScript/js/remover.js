1a799e10b6facec34b1b3aa9d88aec29cda162e1
riq-dev <luis.riq.henrique@gmail.com>
2022-03-17 02:31:09 -0400

    update

--- Módulos-curso/05 - Introdução-JavaScript/js/remover.js
+++ Módulos-curso/05 - Introdução-JavaScript/js/remover.js
@@ -10,9 +10,3 @@ tabela.addEventListener("dblclick", function(event) {
     }, 500);
 
 });
-
-// pacientes.forEach(function(paciente) {
-//     paciente.addEventListener("dblclick", function() {
-//         this.remove();
-//     });
-// });
