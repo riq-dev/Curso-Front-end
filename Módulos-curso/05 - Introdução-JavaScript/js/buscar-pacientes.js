1f8ef251c4a204a8e6683d8ddbdd5a261bc00327
riq-dev <luis.riq.henrique@gmail.com>
2022-03-18 20:11:33 -0400

    v1.0.1

--- Módulos-curso/05 - Introdução-JavaScript/js/buscar-pacientes.js
+++ Módulos-curso/05 - Introdução-JavaScript/js/buscar-pacientes.js
@@ -5,7 +5,7 @@ botaoAdicionar.addEventListener("click", function() {
 
     var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "https://api-pacientes.herokuapp.com/acientes");
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
     xhr.addEventListener("load", function() {
         var erroAjax = document.querySelector("#erro-ajax");
